938528fdecb444b30ccfac674f6db7a9ae754022
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-28 23:48:15 -0800

    05 markov analysis script outline

---
+++ scripts/05_markov_analysis.py
@@ -0,0 +1,8 @@
+# 05_markov_analysis.py
+# Use Markov Chains to model expected number of steps
+
+# Import numpy
+# Define `expected_steps(n)`
+#   - Compute sum of expected steps using recurrence formula
+#   - Compare to simulation results
+# Save results to `outputs/markov_results.csv`
